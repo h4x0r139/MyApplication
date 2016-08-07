6a75b98635ace1e6c5d37b4ecdd3d53b75b038f3
yinxm <h4x0r_001@163.com>
2016-08-07 12:53:02 +0800

    提交

---
+++ viewgroup/src/main/java/com/yinxm/viewgroup/RelativeLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.yinxm.viewgroup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+    }
+}
