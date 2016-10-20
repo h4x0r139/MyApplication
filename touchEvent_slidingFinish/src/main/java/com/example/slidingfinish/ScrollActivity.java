c475ad702d457b62592a2ffa0869d2dcb988b590
yinxm <h4x0r_001@163.com>
2016-10-20 21:12:39 +0800

    更新

---
+++ touchEvent_slidingFinish/src/main/java/com/example/slidingfinish/ScrollActivity.java
@@ -0,0 +1,13 @@
+package com.example.slidingfinish;
+
+import android.os.Bundle;
+
+public class ScrollActivity extends SwipeBackActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_scroll);
+	}
+
+}
