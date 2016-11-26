c9601284fd04f9d291042eee18863ae471818524
yinxm <h4x0r_001@163.com>
2016-11-27 00:10:17 +0800

    测试

--- gridview_test/src/main/java/com/example/testgridview/MainActivity.java
+++ gridview_test/src/main/java/com/example/testgridview/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.testgridview;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.SimpleAdapter;
 
@@ -19,9 +23,9 @@ public class MainActivity extends Activity {
 			R.drawable.camera, R.drawable.clock, R.drawable.games_control,
 			R.drawable.messenger, R.drawable.ringtone, R.drawable.settings,
 			R.drawable.speech_balloon, R.drawable.weather, R.drawable.world,
-			R.drawable.youtube };
-	private String[] iconName = { "通讯录", "日历", "照相机", "时钟", "游戏", "短信", "铃声",
-			"设置", "语音", "天气", "浏览器", "视频" };
+			R.drawable.youtube , R.drawable.ic_launcher};
+	private String[] iconName = {"通讯录", "日历", "照相机", "时钟", "游戏", "短信", "铃声",
+			"设置", "语音", "天气", "浏览器", "视频","测试Main2Activity焦点" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,15 @@ public class MainActivity extends Activity {
 		sim_adapter = new SimpleAdapter(this, data_list, R.layout.item, from, to);
 		//配置适配器
 		gview.setAdapter(sim_adapter);
+		gview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Log.d("yinxm", "onItemClick position="+position);
+				if (position == 12) {
+					startActivity(new Intent(getApplicationContext(), Main2Activity.class));
+				}
+			}
+		});
 	}
 
 	
