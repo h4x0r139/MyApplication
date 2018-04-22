864d3cfdb039cd6b78c3a7ce4ac6e6dbe06af37a
h4x0r139 <h4x0r_001@163.com>
2018-04-22 20:44:36 +0800

    :

---
+++ touchEvent_01/src/main/java/cn/yinxm/tevent/conflict/vp/VpListSlidingConflictActivity.java
@@ -0,0 +1,16 @@
+package cn.yinxm.tevent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * ViewPager+Fragment+list列表滑动冲突
+ */
+public class VpListSlidingConflictActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vp_list_sliding_conflict);
+    }
+}
